2729258ba0ce35d61fc9965f1fafdf13b88da27f
surattugaslppmsmd-dot <surattugaslppmsmd@gmail.com>
2025-09-23 04:32:51 +0700

    Fix routing for admin-login API

--- server.ts
+++ server.ts
@@ -54,7 +54,7 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static(uploadDir));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 // ---------- Multer ----------
 
