af72c9b712f3d0547015fe7f1e9b361e0b964198
surattugaslppmsmd-dot <surattugaslppmsmd@gmail.com>
2025-09-23 09:15:59 +0700

    Update 1

--- server.ts
+++ server.ts
@@ -331,8 +331,4 @@ app.get("/", (req, res) => {
   res.send("API aktif");
 });
 
-// === Server listen ===
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
