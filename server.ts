36be2bd74948fb3ba4b65b7499507e650181d8d1
surattugaslppmsmd-dot <surattugaslppmsmd@gmail.com>
2025-12-02 13:35:44 +0700

    update cors

--- server.ts
+++ server.ts
@@ -463,25 +463,5 @@ if (!process.env.VERCEL) {
 
 // ------------------ VERCEL HANDLER ------------------
 export default function handler(req: any, res: any) {
-  const origin = req.headers.origin || "";
-
-  // CORS hanya di sini (clean)
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Requested-With"
-  );
-
-  // Preflight
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-
   return app(req, res);
 }
