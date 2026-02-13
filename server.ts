5f112f1deebf7d3c0ab13a299c642061b89aebcd
Syafi <surattugaslppmsmd@gmail.com>
2026-02-13 14:11:19 +0800

    update vercel.json dan server

--- server.ts
+++ server.ts
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
-
+app.options("*", (_, res) => res.status(200).end());
 const upload = multer({ storage: multer.memoryStorage() });
 
 // DATABASE
